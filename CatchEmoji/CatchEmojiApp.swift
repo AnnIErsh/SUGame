4e0d1359e9c0956a5b8575e394234b6c1c8c492a
Anna <annaersh1994@gmail.com>
2022-08-08 00:48:39 +0300

    Initial Commit

---
+++ CatchEmoji/CatchEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmojiApp.swift
+//  CatchEmoji
+//
+//  Created by Anna Ershova on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
